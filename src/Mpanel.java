0e0c0ef981a283b48487812f09e32334acc4a484
qwe <qwe@qwe.qwe>
2021-01-28 17:50:14 +0300

    123

--- src/Mpanel.java
+++ src/Mpanel.java
@@ -16,19 +16,15 @@ public class Mpanel extends JPanel{
     public Mpanel(){
         setLayout(null);
         img = new DrawImg(20, 20, "C:\\java 4\\28.01.2021\\light_PNG14440.png");
-        this.timer = new Timer(2, new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                img.x++;
-                img.y++;
-                repaint();
-                if(img.x==780||img.y==780){
-                    timer.stop();
-                }
+        this.timer = new Timer(2, e -> {
+            repaint();
+            img.x++;
+            img.y++;
+            if(img.x==800||img.y==800){
+                img.x = -30;
+                img.y = -30;
             }
         });
-        img.x = 20;
-        img.y = 20;
         timer.start();
     }
 }
