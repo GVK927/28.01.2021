b9943bbb70c693e7baff00af64f4c16821f87a8b
qwe <qwe@qwe.qwe>
2021-01-28 18:07:42 +0300

    Второе задание

--- src/Mpanel.java
+++ src/Mpanel.java
@@ -15,14 +15,33 @@ public class Mpanel extends JPanel{
 
     public Mpanel(){
         setLayout(null);
-        img = new DrawImg(20, 20, "C:\\java 4\\28.01.2021\\light_PNG14440.png");
-        this.timer = new Timer(2, e -> {
-            repaint();
-            img.x++;
-            img.y++;
-            if(img.x==800||img.y==800){
-                img.x = -30;
-                img.y = -30;
+        img = new DrawImg(100, 100, "C:\\java 4\\28.01.2021\\light_PNG14440.png");
+        this.timer = new Timer(2, new ActionListener() {
+            int c = 0;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (c){
+                    case 0:
+                        img.y++;
+                        repaint();
+                        break;
+                    case 1:
+                        img.x++;
+                        repaint();
+                        break;
+                    case 2:
+                        img.y--;
+                        repaint();
+                        break;
+                    case 3:
+                        img.x--;
+                        repaint();
+                        break;
+                }
+                if(img.y == 300&&img.x == 100) c = 1;
+                if(img.y == 300&&img.x == 300) c = 2;
+                if(img.y == 100&&img.x == 300) c = 3;
+                if(img.y == 100&&img.x == 100) c = 0;
             }
         });
         timer.start();
--- src/Window.java
+++ src/Window.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Window extends JFrame {
     public Window(){
-        setBounds(200, 200, 800, 800);
+        setBounds(200, 200, 500, 500);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Первое задание");
